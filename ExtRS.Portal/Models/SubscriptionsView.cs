83a43895c1bd24210a29dd9280ea03a861526f75
colin fitzgerald <cfitzg1983@gmail.com>
2024-02-24 14:14:53 -0600

    Add US Presidential elections small multiple report

--- ExtRS.Portal/Models/SubscriptionsView.cs
+++ ExtRS.Portal/Models/SubscriptionsView.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using ReportingServices.Api.Models;
+using Sonrai.ExtRS.Models.Enums;
 
 namespace ExtRS.Portal.Models
 {
@@ -12,7 +13,7 @@ namespace ExtRS.Portal.Models
         public string ReportServerName;
         public List<Subscription> Subscriptions { get; set; }
         public Subscription SelectedSubscription { get; set; }
-        public override string CurrentTab { get { return _currentTab; } set { _currentTab = value; } }
+        public override string? CurrentTab { get { return _currentTab; } set { _currentTab = value!; } }
     }
 
     public class SubscriptionView : LayoutView
@@ -21,8 +22,17 @@ namespace ExtRS.Portal.Models
         public string ReportServerName;
         public Subscription Subscription { get; set; }
         public List<Report> Reports { get; set; }
+        public List<ParameterValue> ReportParameters { get; set; }
+        public List<RecurrenceType> RecurrenceTypes = new List<RecurrenceType> { RecurrenceType.Hourly, RecurrenceType.Daily, RecurrenceType.Weekly, RecurrenceType.Monthly, RecurrenceType.MonthlyDOW, RecurrenceType.Onetime };
         public string DefaultEmailAddress { get; set; }
-        private string _currentTab;
-        public override string CurrentTab { get { return _currentTab; } set { _currentTab = value; } }
+        public MinuteRecurrence? MinuteRecurrence { get; set; }
+        public DailyRecurrence? DailyRecurrence { get; set; }
+        public WeeklyRecurrence? WeeklyRecurrence { get; set; }
+        public MonthlyRecurrence? MonthlyRecurrence { get; set; }
+        public MonthlyDOWRecurrence? MonthlyDOWRecurrence { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        private string? _currentTab;
+        public override string? CurrentTab { get { return _currentTab; } set { _currentTab = value!; } }
     }
 }
---
+++ ExtRS/Models/Enums/RecurrenceType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Sonrai.ExtRS.Models.Enums
+{
+    public enum RecurrenceType
+    {
+        Hourly = 1,
+        Daily = 2,
+        Weekly = 3,
+        Monthly = 4,
+        MonthlyDOW = 5,
+        Onetime = 6
+    };
+}
